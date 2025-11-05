b6e839fbaa8d3952755a831f3d022bfb718c072a
MrDyslexia <mhhernand@gmail.com>
2025-11-05 16:04:53 -0300

    refactor: Update capacitor.config.ts for consistency and readability; remove unused dependency from package.json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,20 +1,34 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.lowframes.app',
-  appName: 'MarejadasUV',
-  webDir: 'build',
+  appId: "com.lowframes.app",
+  appName: "MarejadasUV",
+  webDir: "build",
   plugins: {
-      StatusBar: {
-        style: 'DEFAULT',
-        overlaysWebView: true,
-        backgroundColor: '#00000000'
-      }
+    StatusBar: {
+      style: "DARK",
+      overlaysWebView: true,
+      backgroundColor: "#00000000",
     },
-    android: {
-      // Para modo inmersivo
-      useLegacyBridge: false
-    }
-  };
+    Keyboard: {
+      resize: "body",
+      style: "DARK",
+      resizeOnFullScreen: true,
+    },
+    SplashScreen: {
+      launchShowDuration: 2000,
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinerStyle: "large",
+      splashFullScreen: true,
+      splashInmersive: true,
+      backgroundColor: "#73cef8ff",
+    },
+  },
+  android: {
+    // Para modo inmersivo
+    useLegacyBridge: false,
+  },
+};
 
 export default config;
