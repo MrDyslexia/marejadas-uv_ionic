6a4a1a52514dfd3d431f9b13dd9c7be49eb71bb7
MrDyslexia <mhhernand@gmail.com>
2025-11-05 15:29:52 -0300

    Refactor code structure for improved readability and maintainability

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.lowframes.app',
   appName: 'MarejadasUV',
-  webDir: 'build'
-};
+  webDir: 'build',
+  plugins: {
+      StatusBar: {
+        style: 'DEFAULT',
+        overlaysWebView: true,
+        backgroundColor: '#00000000'
+      }
+    },
+    android: {
+      // Para modo inmersivo
+      useLegacyBridge: false
+    }
+  };
 
 export default config;
