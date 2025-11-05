a0d259ed469313ad13c0aa68d3af3e090579594e
MrDyslexia <mhhernand@gmail.com>
2025-11-04 22:37:37 -0300

    Refactor code structure for improved readability and maintainability

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lowframes.app',
+  appName: 'MarejadasUV',
+  webDir: 'build'
+};
+
+export default config;
